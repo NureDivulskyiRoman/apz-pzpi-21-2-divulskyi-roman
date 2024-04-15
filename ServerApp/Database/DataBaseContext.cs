675d7cc234411fdfd83b7f84f0af7b5184fada7e
Roman <roman.divulskyi@nure.ua>
2024-04-16 01:15:04 +0200

    Створення файлу бази даних, налаштування у файлах та додавання сваггеру

---
+++ ServerApp/Database/DataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Collections.Specialized.BitVector32;
+
+namespace ServerApp.Database
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext()
+        {
+            // Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AgriFlow;Trusted_Connection=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
