66a7b2c3cdbe1443630cdb4bdc1640fec692cc09
Roman <roman.divulskyi@nure.ua>
2024-05-02 15:15:09 +0200

    Додано файл для маршрутизації та файл з стилями

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { AppRouter } from './components/AppRouter/AppRouter';
+import './styles/App.css';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <AppRouter />
     </BrowserRouter>
   );
 }
---
+++ src/components/AppRouter/AppRouter.tsx
@@ -0,0 +1,14 @@
+import { observer } from "mobx-react-lite";
+import React, { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+//import { applicationRoutes } from "../../routes";
+//import { Navbar } from "../Navbar/Navbar";
+
+export const AppRouter = observer(() => {
+
+  return (
+    <div> 
+
+    </div>
+  );
+});
