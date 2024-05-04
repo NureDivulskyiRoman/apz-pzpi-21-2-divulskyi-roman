056e028c4dcfb5cccc94418e8351bde7d8468fd7
Roman <roman.divulskyi@nure.ua>
2024-05-04 20:30:17 +0200

    Додано навігаційну панель та файл з константами для адресів та маршрутів

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -2,13 +2,13 @@ import { observer } from "mobx-react-lite";
 import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 //import { applicationRoutes } from "../../routes";
-//import { Navbar } from "../Navbar/Navbar";
+import { Navbar } from "../Navbar/Navbar";
 
 export const AppRouter = observer(() => {
 
   return (
-    <div> 
-
+    <div>
+      
     </div>
   );
 });
